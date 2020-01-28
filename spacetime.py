ffc98af6de8876a9e609110cc648c071b7e08350
jonmitten <jonmitten@gmail.com>
2020-01-28 11:26:46 -0800

    Unit Tests Passing
    Unit tests passing for several conversions.

--- spacetime.py
+++ spacetime.py
@@ -1,9 +1,10 @@
 import argparse
 import calendar
+import pytz
 import time
 
-from datetime import datetime, timedelta, timezone
-from dateutil import parser
+from datetime import datetime
+
 
 
 class Times:
@@ -12,6 +13,11 @@ class Times:
         self.TICKS = 315964800  # seconds between gps and unix epoch
         self.parser = argparse.ArgumentParser(description='Process some times.')
         self.parser.add_argument('fmt', metavar='N', type=str, nargs='+', help='Return time in format provided.')
+        self.utc = pytz.timezone('UTC')
+        self.iso_fmt = '%Y-%m-%dT%H:%M:%S'
+
+    def dt_ticks(self):
+        return datetime.fromtimestamp(self.TICKS)
 
     def seconds(self, timestamp):
         """
@@ -22,12 +28,15 @@ class Times:
         if isinstance(timestamp, float):
             return round(timestamp, 0)
         elif isinstance(timestamp, time.struct_time):
-            ts = self.seconds(time.mktime(timestamp))
+            # ts = self.seconds(time.mktime(timestamp))  # trouble maker
+            dt = datetime.fromtimestamp(calendar.timegm(timestamp))
+            ts = (dt - datetime(1970, 1, 1)).total_seconds()
+            print(ts)
             return ts
         elif isinstance(timestamp, int):
             return timestamp
-
-
+        elif isinstance(timestamp, datetime):
+            return timestamp
 
     def current_time(self, fmt='unix'):
         """
@@ -38,17 +47,16 @@ class Times:
         :return:
         """
         if fmt == 'unix':
-            return self.seconds(time.gmtime())
+            cur_time = time.time()
+            return self.seconds(cur_time)
         elif fmt == 'gps':
             return self.seconds(self.gps_time(time.gmtime()))
         elif fmt == 'iso':
             return self.iso_time(self.seconds(time.gmtime()))
 
-
     def unix_time(self, timestamp, fmt='gps'):
         if fmt == 'gps':
             return timestamp + self.TICKS
-
 
     def gps_time(self, timestamp, fmt='unix'):
         """
@@ -64,11 +72,29 @@ class Times:
             elif isinstance(timestamp, float):
                 return self.seconds(timestamp) - self.TICKS
             elif isinstance(timestamp, time.struct_time):
-                return self.seconds(self.intstruct(timestamp)) - self.TICKS
+                ts = self.intstruct(timestamp)
+                ts = self.seconds(ts)
+                return ts - self.TICKS
+        elif fmt == 'iso':
+            if isinstance(timestamp, str):
+                try:
+                    dt = datetime.strptime(datetime, self.iso_fmt)
+                    print(type(dt))
+                    diff = self.dt_ticks()
+                    gps_dt = dt - diff
+                    print(gps_dt)
+                    return gps_dt
+                except TypeError as te:
+                    print('gps_time() had trouble converting from iso to gps timestamp:{}'.format(te))
 
     def intstruct(self, timestamp):
+        """
+        Return a float formatted timestamp from a time.struct_time object.
+        :param timestamp: time.struct_time object - the time to convert
+        :return: float - converted timestamp to seconds since epoch
+        """
         try:
-            return time.mktime(timestamp)
+            return calendar.timegm(timestamp)
         except TypeError as te:
             print('instruct() had a type error: {}'.format(te))
 
@@ -84,17 +110,17 @@ class Times:
             if isinstance(timestamp, float):
                 try:
                     timestamp = self.seconds(timestamp)
-                    return datetime.fromtimestamp(timestamp).isoformat()
+                    return datetime.utcfromtimestamp(timestamp).isoformat()
                 except TypeError as te:
                     print(te)
             if isinstance(timestamp, int):
                 try:
-                    return datetime.fromtimestamp(timestamp).isoformat()
+                    return datetime.utcfromtimestamp(timestamp).isoformat().rsplit('+', 1)[0]
                 except TypeError as te:
-                    print(te)
+                    print('iso_time could not convert due to {}'.format(te))
             if isinstance(timestamp, time.struct_time):
                 try:
-                    ts = time.mktime(timestamp)
+                    ts = calendar.timegm(timestamp)
                     return self.seconds(ts)
                 except TypeError as te:
                     print(te)
--- tests/test_time.py
+++ tests/test_time.py
@@ -1,23 +1,30 @@
 import time
 import unittest
-from datetime import datetime, timezone
+from datetime import datetime
 from spacetime import Times, Spaces
 
 class TimesTestCase(unittest.TestCase):
 
     def setUp(self):
         self.times = Times()
-        self.unix_ts_1 = 233366400  # May 25, 1977
+        self.unix_ts_1 = 233409600  # May 25, 1977 at noon GMT
+        self.iso_star_wars = '1977-05-25T12:00:00'
         self.unix_ts_2 = 327715200  # May 21, 1980
         self.gps_ts_1 = 11750400    # May 21, 1980
+        self.iso_rotj = '1980-05-21T12:00:00'
         self.gps_ts_2 = 1414281600  # Oct 26, 2014
+
 
 
 
     def test_ticks(self):
         self.assertEqual(self.times.TICKS, 315964800)
 
-    def test_current_time(self):
+    def test_current_unix_time(self):
+        """
+        expect integer of seconds since unix epoch
+        :return: integer - seconds since unix epoch
+        """
         t_time = self.times.current_time()
         u_time = time.gmtime()
         u_time = self.times.intstruct(u_time)
@@ -28,11 +35,34 @@ class TimesTestCase(unittest.TestCase):
         u_gps = self.times.current_time(fmt='gps')
 
     def test_current_iso_time(self):
+        # rsplitting the end of these allows for +/- 59 seconds for the test to run.
+        # the test is really asserting that the timezones and dates and times are the same,
+        # so seconds aren't really too relevant here.
         current_iso_time = (datetime.utcnow()
                             .replace(microsecond=0)
-                            .isoformat())
-        t_iso_time = self.times.iso_time(self.times.current_time())
+                            .isoformat()).rsplit(':', 1)[0]
+        t_iso_time = self.times.iso_time(self.times.current_time()).rsplit(':', 1)[0]
+        print(current_iso_time)
+        print(t_iso_time)
         self.assertEqual(current_iso_time, t_iso_time)
+
+    def test_unix_iso_time(self):
+        sw_unix = self.unix_ts_1
+        sw_iso = self.iso_star_wars
+        convert = self.times.iso_time(sw_unix, 'unix')
+        self.assertEqual(convert, sw_iso)
+
+    def test_unix_gps_time(self):
+        rotj_unix = self.unix_ts_2
+        rotj_gps = self.gps_ts_1
+        self.assertEqual(rotj_unix, self.times.unix_time(rotj_gps, 'gps'))
+        self.assertEqual(self.times.gps_time(rotj_unix, 'unix'), rotj_gps)
+
+    def test_gps_iso_time(self):
+        rotj_gps = self.gps_ts_1
+        rotj_iso = self.iso_rotj
+        self.assertEqual(rotj_gps, self.times.gps_time(rotj_iso, 'iso'))
+        self.assertEqual(rotj_iso, self.times.iso_time(rotj_gps, 'gps'))
 
 class SpacesTestCase(unittest.TestCase):
     def setUp(self):
